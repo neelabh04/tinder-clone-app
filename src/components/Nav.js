aaa92cba456f1ec1bf85c3b16cd289a433fb2fb1
Neelabh Singh <neelabh041997@gmail.com>
2022-10-22 15:51:21 +0530

    AuthModal UI Achieved

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,25 @@
 import whiteLogo from '../images/tinder_logo_white.png'
 import colorLogo from '../images/tinder_logo_white.png'
 
-const Nav = ({ minimal, authToken }) => {
+const Nav = ({ minimal, authToken, setShowModal, showModal, setIsSignUp }) => {
+    const handleClick = () => {
+        setShowModal(true)
+        setIsSignUp(false)
+    }
     return (
         <nav>
             <div className="logo-container">
                 <img className="logo" src={minimal ? colorLogo : whiteLogo}/>
             </div>
-            {!authToken && <button className='nav-button'>Log in</button>}
+            {!authToken && !minimal && 
+                <button 
+                    className='nav-button' 
+                    onClick={handleClick}
+                    disabled={showModal}
+                >
+                    Log in
+                </button>
+            }
         </nav>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,46 @@
-import Nav from '../components/Nav'
+import Nav from "../components/Nav";
+import AuthModal from "../components/AuthModal";
+import { useState } from "react";
 
 const Home = () => {
-    const authToken = false
-    const handleClick = () => {
-        console.log('clicked')
-    }
-    return (
-        <div className='overlay'>
-            <Nav minimal={false} authToken={authToken}/>
-            <div className="home">
-                <h1>Swipe Right®</h1>
-                <button className="primary-button" onClick={handleClick}>
-                    {authToken? 'Signout' : 'Create Account'}
-                </button>
-            </div>
-        </div>
-    )
-}
+  const [showModal, setShowModal] = useState(false);
+  const [isSignUp, setIsSignUp] = useState(true);
 
-export default Home
+  const authToken = false;
+
+  const handleClick = () => {
+    console.log("clicked");
+    setShowModal(true);
+    setIsSignUp(true)
+  };
+
+  return (
+    <div className="overlay">
+      <Nav
+        minimal={false}
+        authToken={authToken}
+        setShowModal={setShowModal}
+        showModal={showModal}
+        setIsSignUp={setIsSignUp}
+      />
+
+      <div className="home">
+        <h1 className="primary-title">Swipe Right®</h1>
+        <button className="primary-button" onClick={handleClick}>
+          {authToken ? "Signout" : "Create Account"}
+        </button>
+
+        {showModal && 
+            (
+                <AuthModal 
+                    setShowModal={setShowModal} 
+                    isSignUp={isSignUp}
+                />
+            )
+        }
+      </div>
+    </div>
+  );
+};
+
+export default Home;
